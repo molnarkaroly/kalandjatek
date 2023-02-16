2cd7c64f077d7745e34fede59621255a0bf699be
TothSoma <toth.soma.zsombor@diak.szbi-pg.hu>
2023-02-16 10:37:35 +0100

    szint 15; Tomó sebzés

--- jatek.py
+++ jatek.py
@@ -1,15 +1,17 @@
 import random
 import keyboard
+import json
 
 class DoboKocka:
     def dobokocka():
         return print(random.randint(1,6))
 
-class Tomó(self, name, ügyesség, életerő, szerencse, depresszio, inventory, arany, liquid, csillogo, kaja):
+class Tomó(self, name, ügyesség, életerő, szerencse, sebzés, depresszio, inventory, arany, liquid, csillogo, kaja):
     self.name = name
     self.ügyesség = DoboKocka += 6
     self.életerő = DoboKocka += DoboKocka += 12
     self.szerencse = DoboKocka += 6
+    self.sebzés = 2
     self.depresszio = 0
     self.inventory = 'Vaskard', 'Bőrvért'
     self.arany = Arany
@@ -17,7 +19,8 @@ class Tomó(self, name, ügyesség, életerő, szerencse, depresszio, inventory,
     self.csillogo = Ekkovek
     self.kaja = Kaja
 
-class Enemy(self, name, ügyesség, életerő):
+class Enemy(self, name, ügyesség, életerő, sebzés):
     self.name = name
     self.ügyesség = DoboKocka += DoboKocka += x
     self.életerő = DoboKocka += DoboKocka += y
+    self.sebzés = z
