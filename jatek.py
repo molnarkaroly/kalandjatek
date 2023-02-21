85b0a264e0515e2bc6b8f6fca424682950a67912
molnarkaroly <molnar.karoly@diak.szbi-pg.hu>
2023-02-21 20:08:19 +0100

    harc + javitasok

--- jatek.py
+++ jatek.py
@@ -7,6 +7,7 @@ def DoboKocka():
     dobas = random.randint(1,6)
     return dobas
 
+ugras= []
 
 class Tomó:
     def __init__(self, nev, ugyesseg, eletero, szerencse, depresszio, inventory, arany, liquid, csillogo, kaja):
@@ -21,9 +22,26 @@ class Tomó:
         self.csillogo = csillogo
         self.kaja = kaja
 
+    def harc(self, enemy ):
+        while self.health > 0 and enemy.health > 0:
+            enemy.health -= self.strength
+            if enemy.health <= 0:
+                print(f"{self.name} nyert a harcban.")
+                szoveg = (f'1: {ugras[1]}')
+                if len(ugras) > 1:
+                    szoveg = (f'1: {ugras[1]} 2:{ ugras[2]}' )
+                break
+            self.health -= enemy.strength
+            if self.health <= 0:
+                print(f"{enemy.name} nyert játékod is véget ért")
+                szoveg = (f"{enemy.name} nyert játékod is véget ért")
+                break
+
 class Enemy:
     def __init__(self, nev, ugyesseg):
         pass
         self.name = nev
         self.ugyesseg = DoboKocka + DoboKocka 
         self.eletero = DoboKocka + DoboKocka 
+
+    
