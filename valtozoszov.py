e383363f868d9d6ccbc2e54a413a9a13332d76e4
molnarkaroly <molnar.karoly@diak.szbi-pg.hu>
2023-02-27 19:31:32 +0100

    a json még mindig nem jó de legalább a tkinter halad

---
+++ valtozoszov.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
+import json
+
+
+with open('kaland.json', 'r', encoding='utf-8') as f:
+    kartyak = json.load(f)
+
+class CustomButton(tk.Button):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self['command'] = self.on_button_click
+
+    def on_button_click(self):
+        print(int(self['text']))
+        szint = int(self['text'])
+
+bevezeto= kartyak["bevezeto"]
+szöveg  = "Szeretném közölni a játékossal, hogy ez a játék Tomó halálával végződik legtöbb esetben csak erős tűrőképességel bíróknak ajánljuk ezt a tartalmat."
+szintek = [1,5,6]
+
+class MyApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Kalandjáték")
+        self.master.configure(background="#261401")
+        self.szintek = szintek
+
+        self.szint_szovegek()
+        self.create_buttons()
+
+    def szint_szovegek(self):
+       text = Text(root, height=10, width=100,font=("Arial", 25))
+       text.pack(pady=10)
+       text.insert("end", szöveg)
+
+    def create_buttons(self):
+        for i in self.szintek:
+                button = CustomButton(self.master, text=f"{i}",font=("Arial", 30))
+                button.pack(pady=5)
+
+root = tk.Tk()
+app = MyApp(root)
+root.mainloop()
