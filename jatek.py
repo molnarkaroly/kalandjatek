ef38cd426f27c058faa2cb0fefc173759b43b9d0
molnarkaroly <molnar.karoly@diak.szbi-pg.hu>
2023-02-28 16:44:33 +0100

    folytatasok + kinezet (kekérő)

--- jatek.py
+++ jatek.py
@@ -1,7 +1,16 @@
 import random
 import keyboard
 import json
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+import tkinter as tk
+import json
 
+
+
+with open('kaland.json', 'r', encoding='utf-8') as f:
+    kartyak = json.load(f)
 
 def DoboKocka():
     dobas = random.randint(1,6)
@@ -16,11 +25,11 @@ class Tomó:
         self.eletero = DoboKocka() + DoboKocka() + 12
         self.szerencse = DoboKocka() + 6
         self.depresszio = 0
-        self.inventory = 'Vaskard', 'Bőrvért'
+        self.inventory = inventory['Vaskard', 'Bőrvért']
         self.arany = arany
         self.liquid = liquid
         self.csillogo = csillogo
-        self.kaja = kaja
+        self.kaja = kaja 
 
     def harc(self, enemy ):
         while self.health > 0 and enemy.health > 0:
@@ -47,9 +56,47 @@ class Tomó:
 
 class Enemy:
     def __init__(self, nev, ugyesseg):
-        pass
         self.name = nev
         self.ugyesseg = DoboKocka + DoboKocka 
         self.eletero = DoboKocka + DoboKocka 
 
-    
+
+
+
+
+class CustomButton(tk.Button):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self['command'] = self.on_button_click
+
+    def on_button_click(self):
+        print(int(self['text']))
+        szint = int(self['text'])
+
+bevezeto= kartyak["bevezeto"]
+szöveg  = "Szeretném közölni a játékossal, hogy ez a játék Tomó halálával végződik legtöbb esetben csak erős tűrőképességel bíróknak ajánljuk ezt a tartalmat."
+szintek = [1,5,6]
+
+class MyApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Kalandjáték")
+        self.master.configure(background="#261401")
+        self.szintek = szintek
+
+        self.szint_szovegek()
+        self.create_buttons()
+
+    def szint_szovegek(self):
+       text = Text(root, height=10, width=100,font=("Arial", 25))
+       text.pack(pady=10)
+       text.insert("end", szöveg)
+
+    def create_buttons(self):
+        for i in self.szintek:
+                button = CustomButton(self.master, text=f"{i}",font=("Arial", 30))
+                button.pack(pady=5)
+
+root = tk.Tk()
+app = MyApp(root)
+root.mainloop()
